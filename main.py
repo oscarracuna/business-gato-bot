82c3f3f990ad08de83ed5b888110b2fd440d7c76
oscarracuna <acunainfosec@proton.me>
2024-06-22 14:17:50 -0600

    test

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv, dotenv_values
 from discord import app_commands
 from discord.ext import commands
 
-#test comment
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
